55126ca391c68f33a326e93622c7211852a75b09
Thad R <122906384+thad-rankin@users.noreply.github.com>
2023-09-21 14:30:21 -0500

    Updated JS to fetch and display my public repos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-//profile info will appear here
 const overview = document.querySelector(".overview");
 const username = "thad-rankin";
+const reposList = document.querySelector(".repo-list");
 
 const fetchGitHubUser = async function () {
     const response = await fetch(`https://api.github.com/users/${username}`);
@@ -24,6 +24,22 @@ const displayGitHubUser = function (data) {
     </div>
     `;
     overview.append(div);
+    gitRepos();
 }
 
 fetchGitHubUser();
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepo(repoData);
+}
+
+const displayRepo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = (`<h3>${repo.name}</h3>`);
+        reposList.append(repoItem);
+    }
+}

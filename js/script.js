08a235c75d25f3eab16f3af4acbb7e6e4d60b623
Thad R <122906384+thad-rankin@users.noreply.github.com>
2023-09-26 15:58:34 -0500

    added functionality to allow user to click on a repo title to show that repo's info

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 const overview = document.querySelector(".overview");
 const username = "thad-rankin";
-const reposList = document.querySelector(".repo-list");
+const repoList = document.querySelector(".repo-list");
+const repoContainers = document.querySelector(".repos");
+const repoData = document.querySelector(".repo-data");
 
 const fetchGitHubUser = async function () {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    const data = await response.json();
-    console.log(data);
-    displayGitHubUser(data)
-}
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await userInfo.json();
+  displayGitHubUser(data);
+};
+
+fetchGitHubUser();
 
 const displayGitHubUser = function (data) {
-    const div = document.createElement("div");
-    div.classList.add("user-info");
-    div.innerHTML = `
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
     <figure>
       <img alt="user avatar" src=${data.avatar_url} />
     </figure>
@@ -22,24 +25,60 @@ const displayGitHubUser = function (data) {
       <p><strong>Location:</strong> ${data.location}</p>
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>
-    `;
-    overview.append(div);
-    gitRepos();
-}
-
-fetchGitHubUser();
+  `;
+  overview.append(div);
+  gitRepos();
+};
 
 const gitRepos = async function () {
-    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
-    const repoData = await fetchRepos.json();
-    displayRepo(repoData);
-}
+  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await fetchRepos.json();
+  displayRepos(repoData);
+};
 
-const displayRepo = function (repos) {
-    for (const repo of repos) {
-        const repoItem = document.createElement("li");
-        repoItem.classList.add("repo");
-        repoItem.innerHTML = (`<h3>${repo.name}</h3>`);
-        reposList.append(repoItem);
-    }
-}
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+};
+
+repoList.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    getRepoInfo(repoName);
+  }
+});
+
+const getRepoInfo = async function (repoName) {
+  const fetchRepoInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+  const repoInfo = await fetchRepoInfo.json();
+  console.log(repoInfo);
+  
+  const fetchLanguages = await fetch(repoInfo.languages_url);
+  const languageData = await fetchLanguages.json();
+
+  const languages = [];
+  for (const language in languageData) {
+    languages.push(language);
+  }
+
+  displayRepoInfo(repoInfo, languages);
+};
+
+const displayRepoInfo = function (repoInfo, languages) {
+  repoData.innerHTML = "";
+  repoData.classList.remove("hide");
+  repoContainers.classList.add("hide");
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+  `;
+  repoData.append(div);
+};
